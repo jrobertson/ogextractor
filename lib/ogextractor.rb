5cf49a2d2049a28d8f28be53db07ec5c0f05800b
James Robertson <james@jamesrobertson.me.uk>
2021-04-02 11:13:14 +0100

    1st commit

---
+++ lib/ogextractor.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+
+# file: ogextractor.rb
+# description: Reads Twitter and Open Graph metadata from a given URL.
+
+require 'nokorexi'
+
+
+class OgExtractor
+
+  attr_reader *%i(title description image url type to_h)
+
+  def initialize(article_url, debug: false)
+
+    @debug = debug
+    @properties = %w(title description image url type)
+    @doc = Nokorexi.new(article_url).to_doc
+    extract_metadata()
+
+  end
+
+  private
+
+  def extract_metadata()
+
+    @head = @doc.root.element('head')
+
+    read_og_meta()
+    read_twitter_meta()
+    @url ||= @article_url
+
+    @to_h = if @card then
+
+      {card: @card, title: @title, img: @image, desc: @description, url:  @url}
+              
+    else
+                          
+      if img then
+        
+        {card: @card, title: @title, img: @image, desc: @description, url:  @url}
+
+      elsif title    
+        
+        {card: @card, title: @title, desc: @description, url:  @url}
+      
+      else
+
+        nil
+        
+      end    
+
+    end
+
+  end
+
+  def read_meta(properties=@properties, attrx="property='og")
+
+    properties.each do |x|
+    
+      e = @head.element("meta[@#{attrx}:#{x}']attribute::content")
+      next unless e
+      
+      instance_variable_set('@' + x, e.to_s)
+      
+    end
+
+  end
+
+  def read_og_meta()
+
+    read_meta()
+
+  end
+
+  def read_twitter_meta()
+
+    read_meta(%w(card title description image), "name='twitter")
+
+  end
+
+end
---
+++ ogextractor.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'ogextractor'
+  s.version = '0.1.0'
+  s.summary = 'Reads Twitter and Open Graph metadata from a given URL.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/ogextractor.rb']
+  s.add_runtime_dependency('nokorexi', '~> 0.5', '>=0.5.3')
+  s.signing_key = '../privatekeys/ogextractor.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'digital.robertson@gmail.com'
+  s.homepage = 'https://github.com/jrobertson/ogextractor'
+end
